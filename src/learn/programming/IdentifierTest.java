7fd80d70cbbe7f1889d884536446a0a1babbba1f
Swapna <swapnasairam2007@gmail.com>
2020-03-10 19:31:03 +0000

    Sending identifiers examples to git

---
+++ src/learn/programming/IdentifierTest.java
@@ -0,0 +1,27 @@
+package learn.programming;
+
+public class IdentifierTest {
+    public static void main(String[] args) {
+
+        int x;
+        String myJavaVersion;
+        int 1a;
+        int _1a;
+        int aaa;
+        int main;
+        int static;
+        int void;
+        int String;
+        int 123456a;
+        int $1234567;
+        int _1234567;
+        int 1234567a;
+
+        int do;
+        int Do;
+        int dO;
+
+
+
+    }
+}
